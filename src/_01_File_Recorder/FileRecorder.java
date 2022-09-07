3254cb8b6abbd34769948bfba07ca64031433987
Student <league@iMac-18.attlocal.net>
2022-09-07 16:59:29 -0700

    File Recorder; File Encrypt Decrypt IP

--- src/_01_File_Recorder/FileRecorder.java
+++ src/_01_File_Recorder/FileRecorder.java
@@ -1,5 +1,25 @@
 package _01_File_Recorder;
 
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileRecorder {
 	// Create a program that takes a message from the user and saves it to a file.
+	public static void main(String[] args) {
+		try {
+			FileWriter fw = new FileWriter("src/_01_File_Recorder/output.txt", true);
+			
+			String message = JOptionPane.showInputDialog("What is your message?");
+			
+			fw.write(message + "\n");
+			
+			fw.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
--- src/_02_File_Encrypt_Decrypt/FileEncryptor.java
+++ src/_02_File_Encrypt_Decrypt/FileEncryptor.java
@@ -1,5 +1,12 @@
 package _02_File_Encrypt_Decrypt;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+
+import javax.swing.JOptionPane;
+
 public class FileEncryptor {
 	/*
 	 * Encryption is the process of encoding a message or information
@@ -20,4 +27,32 @@ public class FileEncryptor {
 	 * Create a program that takes a message and a key from the user.
 	 * Use the key to shift each letter in the users input and save the final result to a file.
 	 */
+	
+	public static void main(String[] args) {
+		int key = Integer.parseInt(JOptionPane.showInputDialog("What is your key?"));
+		key = Math.abs(key);
+		key = key % 26;
+		String message = JOptionPane.showInputDialog("What is the message you wish to encode?");
+		char[] chars = message.toCharArray();
+		try {
+			FileWriter fw = new FileWriter("src/_02_File_Encrypt_Decrypt/encrypted.txt");
+			for (char oldChar : chars) {
+				int newCharI = (int)oldChar + key;
+				if (Character.isLowerCase(oldChar)) {
+					newCharI = newCharI > 122 ? newCharI - 26 : newCharI;
+				}else if (Character.isUpperCase(oldChar)) {
+					newCharI = newCharI > 90 ? newCharI - 26 : newCharI;
+				}else {
+					newCharI = (int)oldChar;
+				}
+				char newChar = (char)newCharI;
+				fw.append(newChar);
+			}
+			fw.close();
+			
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
